e983b8031652c565c5a88420bec218704274e5ef
David Aronchick <aronchick@gmail.com>
2022-07-31 19:29:08 -0700

    fixing url downloads

--- dashboards/views.py
+++ dashboards/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import HttpResponse
+from pathlib import Path
 
 
 def index(request):
     return render(request, "index.html")
 
 
-def statsDB(request):
-    jsonFileName = "statsDB.json"
+def rawStats(request):
+    jsonFileName = Path(__file__).parent / "statsDBFile.json"
     with open(jsonFileName, "r") as file:
         response = HttpResponse(file, content_type="application/json")
         response["Content-Disposition"] = f"attachment; filename={jsonFileName}"
--- dashboardsproject/urls.py
+++ dashboardsproject/urls.py
@@ -9,5 +9,6 @@ import dashboards.views
 # To add a new path, first import the app:
 urlpatterns = [
     path("", dashboards.views.index, name="index"),
+    path("rawstats", dashboards.views.rawStats, name="rawstats"),
     path("admin/", admin.site.urls),
 ]
